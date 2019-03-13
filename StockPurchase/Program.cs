7466584bdf6f301b53f74aa899a9e2036b522eb0
MCrank <marco_mcse@hotmail.com>
2019-03-12 21:07:03 -0500

    Add project files.

---
+++ StockPurchase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StockPurchase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
